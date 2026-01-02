101e1668fd4d57c291dc01792bd6a8f2a32c34da
mk09777777 <153955758+mk09777777@users.noreply.github.com>
2026-01-02 23:43:30 +0530

    deploy5

--- resume_scorer/settings.py
+++ resume_scorer/settings.py
@@ -88,9 +88,18 @@ if DEBUG:
     }
 else:
     import dj_database_url
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    if DATABASE_URL:
+        DATABASES = {
+            'default': dj_database_url.parse(DATABASE_URL)
+        }
+    else:
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
 
 
 # Password validation
